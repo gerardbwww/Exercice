d883a439cf8efe4465a4a27fe3d7b788faadb1ad
34011-65-03 <34011-65-03@STA6301484.exchange.ad.afpanet>
2018-11-12 09:27:59 +0100

    enlevé le main

--- src/pckg/Exercice1.java
+++ src/pckg/Exercice1.java
@@ -2,11 +2,11 @@ package pckg;
 
 public class Exercice1 {
 
-	public static void main(String[] args) {
+	public void gerard(){
 		// TODO Auto-generated method stub
 
 		//Start of the program
-		System.out.print("Bonjour cher utilisateur, ceci est votre premier programme!");
+		System.out.print("Bonjour c'est moi Gérard!");
 	}
 
 }
