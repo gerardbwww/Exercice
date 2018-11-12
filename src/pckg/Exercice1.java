41cf555da179944f1e0c844d11b255ae1c4950ae
34011-65-03 <34011-65-03@STA6301484.exchange.ad.afpanet>
2018-11-12 09:34:26 +0100

    Je vais y arriver

--- src/pckg/Exercice1.java
+++ src/pckg/Exercice1.java
@@ -2,11 +2,13 @@ package pckg;
 
 public class Exercice1 {
 
-	public void gerard(){
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		//Start of the program
-		System.out.print("Bonjour c'est moi Gérard!");
+		System.out.print("Bonjour cher utilisateur, ceci est votre premier programme!");
+		Gg gg = new Gg();
+		gg.gerard();
 	}
 
 }
