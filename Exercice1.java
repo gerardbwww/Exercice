0a0ce3bc27eebd459b47784e0b378f9db865bba9
34011-65-03 <34011-65-03@STA6301484.exchange.ad.afpanet>
2018-11-12 09:15:21 +0100

    Ahah

---
+++ Exercice1.java
@@ -0,0 +1,12 @@
+package myPackageFromScratch;
+
+public class Exercice1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Start of the program
+		System.out.print("Bonjour cher utilisateur, ceci est votre premier programme!");
+	}
+
+}
